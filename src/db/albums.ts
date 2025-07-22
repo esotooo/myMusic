be072e80400284623c7aca39ea87ab8e07443033
esotooo <edwinsotos2003@gmail.com>
2025-07-22 16:13:45 -0600

    Redujendo padding del header, organizando las imagenes por carpetas

--- src/db/albums.ts
+++ src/db/albums.ts
@@ -12,104 +12,139 @@ export const Albums : Album[] = [
         "nombre": "Currents",
         "año": "2015",
         "artista": "Tame Impala",
-        "imagen": "./img/currents.jpeg"
+        "imagen": "./img/albums/currents.jpeg"
     },
     {
         "id": 2,
         "nombre": "Wiped Out!",
         "año": "2015",
         "artista": "The Neighbourhood",
-        "imagen": "./img/wipedout.jpeg"
+        "imagen": "./img/albums/wipedout.jpeg"
     },
     {
         "id": 3,
         "nombre": "Thriller",
         "año": "1982",
         "artista": "Michael Jackson",
-        "imagen": "./img/thriller.png"
+        "imagen": "./img/albums/thriller.png"
     },
     {
         "id": 4,
         "nombre": "What's going on",
         "año": "1971",
         "artista": "Marvin Gaye",
-        "imagen": "./img/whats.jpg"
+        "imagen": "./img/albums/whats.jpg"
     },
     {
         "id": 5,
         "nombre": "Radio Kono",
         "año": "2025",
         "artista": "CLUBZ",
-        "imagen": "./img/radioKono.jpeg"
+        "imagen": "./img/albums/radioKono.jpeg"
     },
     {
         "id": 6,
         "nombre": "Polydans",
         "año": "2021",
         "artista": "Roosevelt",
-        "imagen": "./img/polydans.jpg"
+        "imagen": "./img/albums/polydans.jpg"
     },
     {
         "id": 7,
         "nombre": "10th Anniversary",
         "año": "2019",
         "artista": "BLU-SWING",
-        "imagen": "./img/bluswing.jpg"
+        "imagen": "./img/albums/bluswing.jpg"
     },
     {
         "id": 8,
         "nombre": "Nocturne",
         "año": "2012",
         "artista": "Wild Nothing",
-        "imagen": "./img/nocturne.jpg"
+        "imagen": "./img/albums/nocturne.jpg"
     },
     {
         "id": 9,
         "nombre": "Vessel",
         "año": "2012",
         "artista": "Twenty One Pilots",
-        "imagen": "./img/vessel.jpg"
+        "imagen": "./img/albums/vessel.jpg"
     },
     {
         "id": 10,
         "nombre": "Breakfast in America",
         "año": "1979",
         "artista": "Supertramp",
-        "imagen": "./img/breakfast.jpg"
+        "imagen": "./img/albums/breakfast.jpg"
     },
     {
         "id": 11,
         "nombre": "Rumours",
         "año": "1977",
         "artista": "Fleetwood Mac",
-        "imagen": "./img/rumours.jpg"
+        "imagen": "./img/albums/rumours.jpg"
     },
     {
         "id": 12,
         "nombre": "After Hours",
         "año": "2020",
         "artista": "The Weeknd",
-        "imagen": "./img/after.png"
+        "imagen": "./img/albums/after.png"
     },
     {
         "id": 13,
         "nombre": "Louder Than Bombs",
         "año": "1986",
         "artista": "The Smiths",
-        "imagen": "./img/louder.jpeg"
+        "imagen": "./img/albums/louder.jpeg"
     },
     {
         "id": 14,
         "nombre": "Rubber Soul",
         "año": "1965",
         "artista": "The Beatles",
-        "imagen": "./img/rubber.png"
+        "imagen": "./img/albums/rubber.png"
     },
     {
         "id": 15,
         "nombre": "Casiopeia",
         "año": "1979",
         "artista": "Casiopeia",
-        "imagen": "./img/casio.jpg"
+        "imagen": "./img/albums/casio.jpg"
+    },
+    {
+        "id": 16,
+        "nombre": "I Love You",
+        "año": "2013",
+        "artista": "The Neighbourhood",
+        "imagen": "./img/albums/ilove.jpeg"
+    },
+    {
+        "id": 17,
+        "nombre": "Data",
+        "año": "2023",
+        "artista": "Tainy",
+        "imagen": "./img/albums/data.jpeg"
+    },
+    {
+        "id": 18,
+        "nombre": "Songs From The Big Chair",
+        "año": "1984",
+        "artista": "Tears For Fears",
+        "imagen": "./img/albums/tears.jpeg"
+    },
+    {
+        "id": 19,
+        "nombre": "Definitely Maybe",
+        "año": "1994",
+        "artista": "Oasis",
+        "imagen": "./img/albums/oasis.jpg"
+    },
+    {
+        "id": 20,
+        "nombre": "Favourite Worst Nightmare",
+        "año": "2007",
+        "artista": "Arctic Monkeys",
+        "imagen": "./img/albums/arctic.jpeg"
     },
 ]
--- src/db/playlist.ts
+++ src/db/playlist.ts
@@ -10,42 +10,42 @@ export const Playlists : playlist[] = [
         "id": 1,
         "nombre": "Midnight Grooves",
         "url": "https://music.apple.com/us/playlist/midnight-groove/pl.u-BNA6YVbteNozYxM",
-        "portada": "./img/midnigthgrooves.png"
+        "portada": "./img/portadas/midnigthgrooves.png"
     },
     {  
         "id": 2,
         "nombre": "Sounds of the 80's",
         "url": "https://music.apple.com/us/playlist/80s-sounds/pl.u-6mo4ayyI8D9P4o5",
-        "portada": "./img/80's.png"
+        "portada": "./img/portadas/80's.png"
     },
     {  
         "id": 3,
         "nombre": "Ritmo Urbano",
         "url": "https://music.apple.com/us/playlist/ritmo-urbano/pl.u-Ldbqer3s21AW5gl",
-        "portada": "./img/ritmo.png"
+        "portada": "./img/portadas/ritmo.png"
     },
     {  
         "id": 4,
         "nombre": "Rock Latino",
         "url": "https://music.apple.com/us/playlist/rock-latino/pl.u-ZmblxW1FVmXvZd4",
-        "portada": "./img/rock.png"
+        "portada": "./img/portadas/rock.png"
     },
     {  
         "id": 5,
         "nombre": "Pop en Español",
         "url": "https://music.apple.com/us/playlist/pop-en-espa%C3%B1ol/pl.u-Ldbqe0qI21AW5gl",
-        "portada": "./img/pop.png"
+        "portada": "./img/portadas/pop.png"
     },
     {  
         "id": 6,
         "nombre": "EDM",
         "url": "https://music.apple.com/us/playlist/edm/pl.u-Zmblx9jTVmXvZd4",
-        "portada": "./img/edm.png"
+        "portada": "./img/portadas/edm.png"
     },
     {  
         "id": 7,
         "nombre": "Rock / Pop Guatemalteco",
         "url": "https://music.apple.com/us/playlist/rock-pop-guatemalteco/pl.u-PDb4zX5FeE3NJ4x",
-        "portada": "./img/guatemala.png"
+        "portada": "./img/portadas/guatemala.png"
     },
 ]
